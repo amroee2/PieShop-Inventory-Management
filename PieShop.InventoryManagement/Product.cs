b54f196a2e49e81cc1c9933e321106bcfc014ab0
Amro <amroqadadha@gmail.com>
2024-08-12 14:39:37 +0300

    Add partial code for Product.cs and UnitType.cs
    Add members and fields for product.cs and declared enums in UnitType.cs

---
+++ PieShop.InventoryManagement/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShop.InventoryManagement
+{
+    public class Product
+    {
+        private int id;
+        private string name = string.Empty;
+        private string ?description;
+        private int maxItemsInStock = 0;
+        private UnitType unitType;
+        private int amountInStock = 0;
+        private bool isBelowStockThreshold = false;
+    }
+}
