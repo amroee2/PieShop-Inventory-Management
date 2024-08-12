05e7830145f19e49b5d500183f57c375ec3e85e2
Amro <amroqadadha@gmail.com>
2024-08-12 15:37:37 +0300

    Add functionalities and properties in Product.cs
    Added all required functionalities for the program, may be updated later. Also included the appropriate properities for most data members.

--- PieShop.InventoryManagement/Product.cs
+++ PieShop.InventoryManagement/Product.cs
@@ -8,12 +8,94 @@ namespace PieShop.InventoryManagement
 {
     public class Product
     {
-        private int id;
-        private string name = string.Empty;
-        private string ?description;
         private int maxItemsInStock = 0;
-        private UnitType unitType;
-        private int amountInStock = 0;
-        private bool isBelowStockThreshold = false;
+
+        public int Id
+        {
+            get { return Id; }
+            set
+            {
+                Id = value;
+            }
+        }
+        public int Name
+        {
+            get { return Name; }
+            set
+            {
+                Name = value;
+            }
+        }
+        public string? Description
+        {
+            get { return Description; }
+            set
+            {
+                Description = value;
+            }
+        }
+        public UnitType UnitType { get; set; }
+        public int AmountInStock { get; private set; }
+        public bool IsBelowStockThreshold { get; private set; }
+
+        public void UseItem(int amount)
+        {
+            if (amount > AmountInStock)
+            {
+                Log($"Not enough amount in stock\n");
+                ShortDecription();
+            }
+            else
+            {
+                Log($"Product used successfull\n");
+                ShortDecription();
+                AmountInStock -= amount;
+                UpdateLowStock();
+
+            }
+        }
+        public void AddIntoStock(int amount)
+        {
+            AmountInStock += amount;
+        }
+        private void UpdateLowStock()
+        {
+            if (AmountInStock <= 10)
+            {
+                this.IsBelowStockThreshold = true;
+            }
+            LongDescription();
+        }
+
+        private void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+        private void DecreaseStock(int items, string reason)
+        {
+            if(AmountInStock >= items)
+            {
+                AmountInStock -= items;
+            }
+            else
+            {
+                AmountInStock = 0;
+            }
+            UpdateLowStock();
+            Log(reason);
+        }
+        public void ShortDecription()
+        {
+            Log($"Product {Name} with ID {this.Id}");
+        }
+        public void LongDescription()
+        {
+            Log($"Product: {this.Name}\nID: {this.Id}\nDescription: {this.Description}\nAmount in stock: {AmountInStock}\n" +
+                $"Maximum items this product can have: {maxItemsInStock}\n");
+            if (IsBelowStockThreshold)
+            {
+                Log("STOCK IS LOW!!");
+            }
+        }
     }
 }
