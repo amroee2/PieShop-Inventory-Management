ecf45f97756c4ea2bb748c7394cba39f32791bb6
Amro <amroqadadha@gmail.com>
2024-08-14 12:16:09 +0300

    Create Utilities folder and Order, Product Utilities.

---
+++ PieShop.InventoryManagement/Utilities/GeneralUtilities.cs
@@ -0,0 +1,52 @@
+﻿using PieShop.InventoryManagement.General;
+using PieShop.InventoryManagement.OrderManagement;
+using PieShop.InventoryManagement.ProductManagement;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShop.InventoryManagement.Utilities
+{
+    public class GeneralUtilities
+    {
+        public static void ShowMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome!\n1: Inventory Management\n2: Order Management\n3: Settings\n" +
+    "4: Save all data\n0: Close Application");
+                int operation = Convert.ToInt32(Console.ReadLine());
+                switch (operation)
+                {
+                    case 0:
+                        Console.WriteLine("Program closed");
+                        return;
+                    case 1:
+                        ProductUtilities.IneventoryManagementOperation();
+                        break;
+                    case 2:
+                        OrderUtilities.OrderManagementOperation();
+                        break;
+                    case 3:
+                        Settings();
+                        break;
+                    case 4:
+                        SaveAllData();
+                        break;
+                }
+            }
+        }
+        public static void Settings()
+        {
+
+        }
+        public static void SaveAllData()
+        {
+
+        }
+    }
+}
---
+++ PieShop.InventoryManagement/Utilities/ProductUtilities.cs
@@ -0,0 +1,146 @@
+﻿using PieShop.InventoryManagement.General;
+using PieShop.InventoryManagement.ProductManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShop.InventoryManagement.Utilities
+{
+    public class ProductUtilities
+    {
+        private static List<Product> Products = new List<Product>();
+        public static void IneventoryManagementOperation()
+        {
+            Console.Clear();
+            while (true)
+            {
+                Console.WriteLine("Inventory Management\n");
+                foreach (Product product in Products)
+                {
+                    product.ShortDescription();
+                }
+                Console.WriteLine("1: View details of a product\n2: Add a new product\n" +
+                    "3: Clone product\n4: View all Products details\n0: Back to main menu");
+                int operation = Convert.ToInt32(Console.ReadLine());
+                switch (operation)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        ViewProductDetails();
+                        break;
+                    case 2:
+                        AddNewProduct();
+                        break;
+                    case 4:
+                        ViewAllProducts();
+                        break;
+                }
+            }
+        }
+        public static void ViewProductDetails()
+        {
+            Console.WriteLine("Product id:\n");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Product selectedProduct = new Product();
+
+            foreach (Product product in Products)
+            {
+                if (product.Id == id)
+                {
+                    selectedProduct = product;
+                    selectedProduct.LongDescription();
+                    break;
+                }
+            }
+            Console.WriteLine("What do you want to do\n1- Use Product\n2- Go Back");
+            int op = Convert.ToInt32(Console.ReadLine());
+            switch (op)
+            {
+                case 2: return;
+                case 1:
+                    Console.WriteLine("Enter the amount");
+                    int amount = Convert.ToInt32(Console.ReadLine());
+                    selectedProduct.UseItem(amount);
+                    break;
+            }
+        }
+        private static void AddNewProduct()
+        {
+            Console.WriteLine("Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Maximum items in stock:");
+            int maxItems = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Description:");
+            string? description = Console.ReadLine();
+
+            UnitType unitType;
+            while (true)
+            {
+                Console.Write("Enter the unit type (PerItem, PerBox, PerKg): ");
+                string input = Console.ReadLine();
+
+                if (Enum.TryParse(input, true, out unitType))
+                {
+                    Console.WriteLine($"You selected: {unitType}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid unit type entered. Please try again.");
+                }
+            }
+
+            Console.WriteLine("\nPrice:");
+            int price = Convert.ToInt32(Console.ReadLine());
+
+            Currency currency;
+            while (true)
+            {
+                Console.Write("Enter the currency (Dollar, Euro, Pound): ");
+                string input = Console.ReadLine();
+
+                if (Enum.TryParse(input, true, out currency))
+                {
+                    Console.WriteLine($"You selected: {currency}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid currency entered. Please try again.");
+                }
+            }
+
+            Product product = new Product(id, name, description, unitType, new Price(price, currency), maxItems);
+            product.LongDescription();
+            Products.Add(product);
+        }
+
+        private static void ViewAllProducts()
+        {
+            foreach (Product product in Products)
+            {
+                product.LongDescription();
+            }
+        }
+        public static Product ProductID(int id)
+        {
+            foreach (Product product in Products)
+            {
+                if (product.Id == id)
+                {
+                    return product;
+                }
+            }
+
+            return null;
+        }
+    }
+}
