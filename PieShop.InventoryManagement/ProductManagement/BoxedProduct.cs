ca83922da7066daa5dd61c67d505660ff6026729
Amro <amroqadadha@gmail.com>
2024-08-15 11:23:18 +0300

    Add new product type classes with inheritance functionalities

---
+++ PieShop.InventoryManagement/ProductManagement/BoxedProduct.cs
@@ -0,0 +1,44 @@
+﻿using PieShop.InventoryManagement.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShop.InventoryManagement.ProductManagement
+{
+    public class BoxedProduct : Product
+    {
+        private int amountPerBox;
+
+        public int AmountPerBox
+        {
+            get { return amountPerBox; }
+            set {
+                amountPerBox = value; 
+            }
+        }
+
+
+        public BoxedProduct(int id, string name, string? description, UnitType unitType, Price price, int maxItemsInStock, int amountPerBox) : base(id, name, description, UnitType.PerBox, price, maxItemsInStock)
+        {
+            AmountPerBox = amountPerBox;
+        }
+
+        public void UseBoxedProduct(int items)
+        {
+            int smallestMultiple = 0;
+            int batchsize;
+            while (true)
+            {
+                smallestMultiple++;
+                if (smallestMultiple * AmountPerBox > items)
+                {
+                    batchsize = AmountPerBox * smallestMultiple;
+                    break;
+                }
+            }
+            UseItem(batchsize);
+        }
+    }
+}
---
+++ PieShop.InventoryManagement/ProductManagement/BulkedProduct.cs
@@ -0,0 +1,16 @@
+﻿using PieShop.InventoryManagement.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShop.InventoryManagement.ProductManagement
+{
+    public class BulkedProduct : Product
+    {
+        public BulkedProduct(int id, string name, string? description, UnitType unitType, Price price, int maxItemsInStock) : base(id, name, description, unitType, price, maxItemsInStock)
+        {
+        }
+    }
+}
